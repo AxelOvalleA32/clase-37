1b77457e361c672b8494c44e20112c70cdf530ec
AxelOvalleA32 <115324377+AxelOvalleA32@users.noreply.github.com>
2022-10-07 18:00:06 -0500

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,35 @@
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder","introduce tu nombre");
+    this.titleImg = createImg("assets/title.png", "título del juego");
+    this.playButton = createButton("jugar");
+    this.greeting = createElement("h2");
+  }
+  setElementsposition(){
+    this.input.position(width/2-110, height/2-80);
+    this.titleImg.position(120, 10);
+    this.playButton.position(width/2-90, height/2-20);
+    this.greeting.position(width/2-300, height/2-100);
+  }
+  hide(){
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+  }
+  handMousePressed(){
+    this.playButton.mousePressed(()=>{
+      this.input.hide();
+      this.playButton.hide();
+      var message = ` Hola ${this.input.value()}
+      </br>Espera a que se una otro jugador...`;
+      this.greeting.html(message);
+      player.name = this.input.value();
+      player.index = 1;
+    })
+  }
+  display(){
+    this.setElementsposition();
+    this.handMousePressed();
+  }
+}
+
